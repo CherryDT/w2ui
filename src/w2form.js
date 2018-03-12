2d839eb654b0638219e8106893086edf254a1465
David Trapp <david@dotlabs.co>
2018-03-12 03:10:15 +0100

    Add new feature to allow onLoad and onValidate for specific fields

--- src/w2form.js
+++ src/w2form.js
@@ -388,6 +388,7 @@
                 if (field.equalto && this.record[field.name] != this.record[field.equalto]) {
                     errors.push({ field: field, error: w2utils.lang('Field should be equal to ') + field.equalto });
                 }
+                if (field.onValidate) this.record[field.name] = field.onValidate(field.$el.closest('[data-field]'), field, val, errors);
             }
             // event before
             var edata = this.trigger({ phase: 'before', target: this.name, type: 'validate', errors: errors });
@@ -1263,6 +1264,7 @@
                         $(field.el).w2field($.extend({}, field.options, { type: field.type }));
                         break;
                 }
+                if (field.onLoad) field.onLoad(field.$el.closest('[data-field]'), field, value);
             }
             // wrap pages in div
             var tmp = $(this.box).find('.w2ui-page');
